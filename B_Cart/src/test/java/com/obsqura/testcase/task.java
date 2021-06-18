8bd236647f092f04143b9fc5899c6fd988d4479c
preethipathrose <Preethi40@gmail.com>
2021-06-18 23:50:21 +0530

    Update task.java
    task update

--- B_Cart/src/test/java/com/obsqura/testcase/task.java
+++ B_Cart/src/test/java/com/obsqura/testcase/task.java
@@ -1,5 +1,7 @@
 package com.obsqura.testcase;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -8,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import com.obsqura.page.DashboardPage;
@@ -45,17 +48,19 @@ public class task {
 		
 		genobj.sendKeys(loginpge.username,"admin");
 		genobj.sendKeys(loginpge.password,"123456");
-		genobj.click(loginpge.SignIn);
+		genobj.clickButton(loginpge.SignIn);
 		//List<WebElement> rows=(List<WebElement>) driver.findElements(By.tagName("tr"));
+		//click task tab on dashboard
+		genobj.clickButton(dashbrd.tasksTab);
+		
 	}
 	
 	@Test(priority=1)
 	public void newTask() throws IOException {
 		
-		//click task tab on dashboard
-		genobj.click(dashbrd.tasksTab);
+		
 		//click new project tab on projects section
-		genobj.click(taskpge.newTaskTab);
+		genobj.clickButton(taskpge.newTaskTab);
 		
 		genobj.sendKeys(taskpge.taskName,exlobj.readExcelFun(2,1,"Task"));
 		genobj.sendKeys(taskpge.relation,exlobj.readExcelFun(3,1,"Task"));
@@ -65,10 +70,53 @@ public class task {
 		genobj.sendKeys(taskpge.estimatedHours,exlobj.readExcelFun(8,1,"Task"));
 		genobj.sendKeys(taskpge.taskStatus,exlobj.readExcelFun(9,1,"Task"));
 		genobj.sendKeys(taskpge.taskDescription,exlobj.readExcelFun(10,1,"Task"));
-		genobj.click(taskpge.save);
+		genobj.clickButton(taskpge.save);
 		
+		try {
+			
+		
+		Assert.assertEquals(exlobj.readExcelFun(8,1,"Task"), taskpge.TaskNameView.getText());
+		}
+		
+		catch (Exception e) {
+			
+			obj.screenshot("task_creation");
+		}
 	}
 	
+	@Test(priority=2)
+	public void changeStatus() throws IOException {
+		try {
+			
+		String statusFromExcel=exlobj.readExcelFun(14,1,"Task");
+		switch(statusFromExcel) 
+		{
+			case "On Hold": genobj.clickButton(taskpge.onholdStatus);
+				break;
+			case "In Progress": genobj.clickButton(taskpge.inProgressStatus);
+				break;
+			case "Completed": genobj.clickButton(taskpge.completedStatus);
+				break;
+			case "Not Started": genobj.clickButton(taskpge.notStartedStatus);
+				break;
+			case "Deferred": genobj.clickButton(taskpge.deferredStatus);
+				break;
+			case "Waiting For Someone": genobj.clickButton(taskpge.waitingStatus);
+				break;
+			default:
+				break;
+		}
+		
+		
+			
+			Assert.assertEquals(exlobj.readExcelFun(14,1,"Task"),taskpge.newStatus.getText());	
+		}
+		
+		catch (Exception e) {
+			
+			obj.screenshot("task_statusCheck");
+		}
+	}
 
 	@AfterClass
 	void afterclass() {
